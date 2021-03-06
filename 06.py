574a9550aad6463b6fda2592f6f6a014034d8840
Chiung0101 <77521208+Chiung0101@users.noreply.github.com>
2021-03-06 11:40:01 +0800

    Add files via upload

---
+++ 06.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  6 10:12:27 2021
+
+@author: Ailin
+"""
+
+from mcpi.minecraft import Minecraft 
+mc=Minecraft.create()
+
+while True:
+        hits=mc.events.pollBlockHits()
+        
+        if len(hits)>0:
+            hit=hits[0]
+            x,y,z=hit.pos.x, hit.pos.y, hit.pos.z
+            block=mc.getBlock(x,y,z)
+            mc.postToChat("Block ID: "+str(block))
+            if block==1:
+                mc.setBlock(x,y,z,41)
+                
+                
+                
+x,y,z,=mc.player.getPos()
+mc.setSign(x,y,z,63,0,"1","2","3","4")
+           
+
+
+from mcpi.minecraft import Minecraft 
+mc=Minecraft.create()
+import random
+import time
+
+i=1
+while i<20:
+    pos=mc.player.getPos()
+    i=i+1
+    x=pos.x+random.uniform(-20,20)
+    y=pos.y+random.uniform(3,0)
+    z=pos.z+random.uniform(-20,20)
+    
+    mc.spawnEntity(x,y,z,93)
+    time.sleep(0.1)
+
+
+      
